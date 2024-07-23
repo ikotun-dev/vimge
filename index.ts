2eefed93ca9c29831ee81f644c75ae6913a0c2f9
ikotun-dev <danlogan2003@gmail.com>
2024-07-23 22:52:31 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  // res.send("Hello World");
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
